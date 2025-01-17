6278bccbb59bb2aa67984e13e61a31c1880b6033
BobsonGomez <119694670+BobsonGomez@users.noreply.github.com>
2025-01-17 11:03:35 +0530

    Add files via upload

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:zen/pages/auth_page.dart';
+import 'package:zen/pages/todo.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -14,19 +15,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () => logoutUser(context),
-              icon: Icon(Icons.logout_outlined)
-          )
-        ],
-      ),
-      body: Center(
-          child: Text(
-        "WELCOME TO ZENUNNI",
-        style: TextStyle(fontSize: 24, color: Colors.amber),
-      )),
-    );
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () => logoutUser(context),
+                icon: Icon(Icons.logout_outlined))
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "WELCOME TO ZENUNNI",
+                style: TextStyle(fontSize: 24, color: Colors.amber),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (context) => Todo()));
+                },
+                child: Text("Go to to-do list page"),
+              ),
+            ],
+          ),
+        ));
   }
 }
