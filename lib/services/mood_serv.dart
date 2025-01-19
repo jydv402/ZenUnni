44e0ebfbfcdddc27c354a728bb556b4824a04e76
jydv402 <jydv402@gmail.com>
2025-01-19 23:28:20 +0530

    Modified the add to firestore function

--- lib/services/mood_serv.dart
+++ lib/services/mood_serv.dart
@@ -1,9 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> addMoodToFirestore(String mood) async {
+  final moodDoc =
+      FirebaseFirestore.instance.collection('mood'); //Get the mood collection
   final now = DateTime.now();
-  await FirebaseFirestore.instance.collection('mood').add({
-    'mood': mood,
-    'updatedOn': now,
-  });
+  final today = DateTime(now.year, now.month, now.day);
+
+  // Check if a document for today already exists
+  final query = await moodDoc
+      .where('updatedOn', isGreaterThanOrEqualTo: today)
+      .where('updatedOn', isLessThan: today.add(const Duration(days: 1)))
+      .limit(1)
+      .get();
+
+  // If a document for today exists, query.docs will be non empty
+  if (query.docs.isNotEmpty) {
+    final docId = query.docs.first.id; //Get the document ID
+    await moodDoc.doc(docId).update({
+      'mood': mood,
+      'updatedOn': now
+    }); //Update the mood and updatedOn fields
+  } else {
+    // Document doesn't exist, create a new one
+    await moodDoc.add({
+      'mood': mood,
+      'updatedOn': now,
+    });
+  }
 }
