469f04e4fc5110c41369d8e0e7e488ee7bdf29a6
Amina Fayaz <aminafayaz1077@gmail.com>
2025-01-20 16:19:40 +0530

    moved todo_withlist content to todo

--- lib/pages/todo.dart
+++ lib/pages/todo.dart
@@ -1,153 +1,255 @@
 import 'package:flutter/material.dart';
 import 'package:zen/pages/home_page.dart';
 
+//where data of each task is stored when user is selecting options
+class Task {
+  String name;
+  String description;
+  DateTime date;
+  String priority;
+  //constructor
+  Task({
+    required this.name,
+    required this.description,
+    required this.date,
+    required this.priority,
+  });
+}
+
 class Todo extends StatefulWidget {
   const Todo({super.key});
 
   @override
   State<Todo> createState() => _TodoState();
 }
-
+ 
 class _TodoState extends State<Todo> {
-
-
-  DateTime selectedDate = DateTime.now();
-  TimeOfDay selectedTime = TimeOfDay.now();
-
-  Future<void> selectDate(BuildContext context) async {
-    final DateTime? pickedDate = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: DateTime.now(),
-        lastDate: DateTime(2100));
-    if (pickedDate != null && pickedDate != selectedDate)
-      setState(() {
-        selectedDate = pickedDate;
-      });
+  //variables to store data
+  DateTime? _date; //save date
+  TimeOfDay? _time; //save time
+  String _prior = ""; //save current prior
+  List<Task> tasks = []; //to store the tasks in a list
+  //controllers to pick up the info to be stored
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController descController = TextEditingController();
+  @override
+  void dispose() {
+    nameController.dispose();
+    descController.dispose();
+    super.dispose();
   }
 
-  Future<void> selectTime(BuildContext context) async {
-    final TimeOfDay? pickedTime =
-        await showTimePicker(context: context, initialTime: selectedTime);
-    if (pickedTime != null && pickedTime != selectedTime)
-      setState(() {
-        selectedTime = pickedTime;
-      });
-  }
-  //function for displaying creation of a new task
-  void _showTask(BuildContext context) {
+  //function that displays dialog box to select task details
+  void _showTask(BuildContext context,Function addTaskCallback) {
+    //clearing controllers so that dialog box looks new
+    nameController.clear();
+    descController.clear();
     showDialog(
         context: context,
         barrierDismissible: false,
         useSafeArea: false,
         builder: (BuildContext context) {
-          String _currentprior = "";
-          return SimpleDialog(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            contentPadding: const EdgeInsets.all(30),
-            title: const Text(
-              "Add a new task",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            children: [
-              TextFormField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                    label: Text(
-                      "Task name",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    hintText: "Enter a name for your task"),
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: descController,
-                decoration: const InputDecoration(
-                    label: Text(
-                      "Task description",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    hintText: "Enter a name for your task"),
-              ),
-              const SizedBox(height: 25),
-              //picking a date
-              ElevatedButton(
-                  onPressed: () {
-                    selectDate(context);
-                  },
-                  child: const Text("Select a date")),
-              const SizedBox(height: 25),
-              //picking a time
-              ElevatedButton(
-                  onPressed: () {
-                    selectTime(context);
-                  },
-                  child: const Text("Select a time")),
-              const SizedBox(height: 20),
-              //to select priority of the task
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          DateTime? localDate;
+          TimeOfDay? localTime;
+          String localPrior = ""; //to display current prior on dialog
+          return StatefulBuilder(
+            //stateful builder to display updating elements
+            builder: (BuildContext context,
+                void Function(void Function()) setState) {
+              return SimpleDialog(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                contentPadding: const EdgeInsets.all(30),
+                title: const Text(
+                  "Add a new task",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
                 children: [
-                  const Text("Select priority"),
-                  DropdownButton(
-                      iconSize: 30,
-                      menuWidth: 100,
-                      //menuMaxHeight: 10,
-                      itemHeight: 50,
-                      items: ['High', 'Medium', 'Low']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(
-                            value,
-                            style: const TextStyle(fontSize: 15),
-                          ),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          _currentprior = newValue!;
-                        });
+                  TextFormField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                        label: Text(
+                          "Task name",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        hintText: "Enter a name for your task"),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: descController,
+                    decoration: const InputDecoration(
+                        label: Text(
+                          "Task description",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        hintText: "Enter a name for your task"),
+                  ),
+                  const SizedBox(height: 25),
+                  //picking a date
+                  ElevatedButton(
+                      onPressed: () async {
+                        DateTime? pickDate = await showDatePicker(
+                            context: context,
+                            firstDate: DateTime.now(),
+                            lastDate: DateTime(2100));
+                        if (pickDate != null) {
+                          setState(() {
+                            localDate = pickDate; //saves to local variable
+                            _date = pickDate; //to save it to global variable
+                            print("Selected date: $localDate");
+                          });
+                        }
                       },
-                      underline: Container()),
-                ],
-              ),
-              const SizedBox(height: 90),
-              //to add buttons which confirms creation of a task or to discards
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FloatingActionButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Icon(
-                      Icons.close,
-                      size: 25,
-                      color: Colors.red,
+                      child: const Text("Select a date")),
+                  const SizedBox(height: 15),
+                  if (localDate != null)
+                    Text(
+                      "Selected Date: ${localDate!.year}-${localDate!.month.toString().padLeft(2, '0')}-${localDate!.day.toString().padLeft(2, '0')}", // Display selected date
+                      style: const TextStyle(fontSize: 18),
+                    )
+                  else
+                    const Text(
+                      "No date selected currently", // Display fallback message if no date is selected
+                      style:
+                          TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
                     ),
+                  //picking a time
+                  ElevatedButton(
+                      onPressed: () async {
+                        TimeOfDay? picktime = await showTimePicker(
+                            context: context, initialTime: TimeOfDay.now());
+                        if (picktime != null) {
+                          setState(() {
+                            localTime = picktime; //saves to local variable
+                            _time = picktime; //saves to global variable
+                            print("Selected time: $localTime");
+                          });
+                        }
+                      },
+                      child: const Text("Select a time")),
+                  const SizedBox(height: 20),
+                  if (localTime != null)
+                    Text(
+                      "Selected Time: ${localTime!.hour}:${localTime!.minute.toString().padLeft(2, '0')}",
+                      style: const TextStyle(fontSize: 18),
+                    )
+                  else
+                    const Text(
+                      "No time selected currently",
+                      style:
+                          TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+                    ),
+                  const SizedBox(height: 20),
+                  //to select priority of the task
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text("Select priority"),
+                      DropdownButton(
+                          value: localPrior.isEmpty ? null : localPrior,
+                          iconSize: 30,
+                          menuWidth: 100,
+                          //menuMaxHeight: 10,
+                          itemHeight: 50,
+                          dropdownColor: Colors.transparent,
+                          items: ['High', 'Medium', 'Low']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: const TextStyle(fontSize: 15),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              localPrior = newValue!;
+                              _prior = newValue;
+                              print(_prior);
+                            });
+                          },
+                          underline: Container()),
+                    ],
                   ),
-                  FloatingActionButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child:
-                        const Icon(Icons.check, size: 25, color: Colors.green),
-                  ),
+                  const SizedBox(height: 90),
+                  //to add buttons which confirms creation of a task or to discards
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      FloatingActionButton(
+                        onPressed: () {
+                          // X has been pressed so removing whatever has been stored
+                          _date = null;
+                          _time = null;
+                          _prior = "";
+                          nameController.text = "";
+                          descController.text = "";
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.close,
+                          size: 25,
+                          color: Colors.red,
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed: () {
+                          if (
+                            nameController.text.isNotEmpty &&
+                              descController.text.isNotEmpty &&
+                              _date != null &&
+                              _time != null &&
+                              _prior.isNotEmpty
+                              ) {
+                            setState(
+                              () {
+                                //this will combine both date and time into date_time
+                                DateTime dateTime = DateTime(
+                                    _date!.year,
+                                    _date!.month,
+                                    _date!.day,
+                                    _time!.hour,
+                                    _time!.minute);
+                                //adding details to task model
+                                Task task = Task(
+                                    name: nameController.text,
+                                    description: descController.text,
+                                    date: dateTime,
+                                    priority: _prior);
+                                tasks.add(task);
+                                //setting the global variables to null
+                                _date = null;
+                                _time = null;
+                                _prior = "";
+                                nameController.text = "";
+                                descController.text = "";
+                                //debugging stuff can be removed later
+                                print("new task has been added successfully");
+                                for (var t in tasks) {
+                                  print(
+                                      "Task: ${t.name}, Description: ${t.description}, "
+                                      "Date: ${t.date}, Priority: ${t.priority}");
+                                }
+                              },
+                            );
+                            Navigator.pop(context);
+                          } else {
+                            print("error fields must be null");
+                          }
+                        },
+                        child: const Icon(Icons.check,
+                            size: 25, color: Colors.green),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
+              );
+            },
           );
         });
   }
-
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController descController = TextEditingController();
-  final TextEditingController dateController = TextEditingController();
-  final TextEditingController priorityController = TextEditingController();
-
-  List<Map<String, String>> _taskList = [];
 
   @override
   Widget build(BuildContext context) {
@@ -175,6 +277,8 @@ class _TodoState extends State<Todo> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [],
         ),
+
+       
         bottomNavigationBar: Padding(
           padding: const EdgeInsets.all(20),
           child: Row(
