c9d63d2cc69bcc28c5b2439414e116447602c82d
Amina Fayaz <aminafayaz1077@gmail.com>
2025-02-20 18:40:36 +0530

    added update function

--- lib/services/habit_serv.dart
+++ lib/services/habit_serv.dart
@@ -27,8 +27,6 @@ final habitProvider = StreamProvider<List<HabitModel>>((ref) async* {
   }
 });
 
-
-
 final habitAddProvider =
     FutureProvider.autoDispose.family<void, HabitModel>((ref, habit) async {
   final FirebaseAuth auth = FirebaseAuth.instance;
@@ -39,3 +37,22 @@ final habitAddProvider =
   await habitDoc.add(habit.toMap());
 });
 
+final habitUpdateProvider = FutureProvider.autoDispose.family<void, HabitModel>((ref, habit) async {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final habitDoc = FirebaseFirestore.instance
+      .collection('users')
+      .doc(auth.currentUser?.uid)
+      .collection('habit');
+
+
+    //Todo: update query condition
+  final querySnapshot = await habitDoc
+      .where('habitName', isEqualTo: habit.habitName)
+      .get();
+      
+  if (querySnapshot.docs.isNotEmpty) {
+    await querySnapshot.docs.first.reference.update(habit.toMap());
+  } else {
+    throw Exception('Habit Not Found');
+  }
+});
