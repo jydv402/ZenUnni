e50f15cbf948249537f8bffa3ba3856f10f6528e
jydv402 <jydv402@gmail.com>
2025-03-08 16:20:39 +0530

    Email verify page UI revamp

--- lib/auth_pages/username.dart
+++ lib/auth_pages/username.dart
@@ -22,6 +22,7 @@ class UsernamePage extends ConsumerWidget {
           const SizedBox(height: 60),
           TextField(
             controller: userNameController,
+            style: Theme.of(context).textTheme.bodySmall,
             decoration: const InputDecoration(
               labelText: 'Username',
             ),
